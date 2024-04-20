3807df88eab65bd8dc1d3deddbebcf20e16965de
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-04-19 20:50:30 -0500

    Create header.js

---
+++ public/scripts/header.js
@@ -0,0 +1,20 @@
+document.write(`
+<header>
+ <div id="header-left">
+   <a href="/" class="header-title">Oblivion</a>
+ </div>
+ <div id="header-center">
+ <div class="center-links">
+   <a href="/apps.html" title="Apps"><i class="fas fa-tablet-screen-button"></i></a>
+   <a href="/games.html" title="Games"><i class="fas fa-gamepad"></i></a>
+   <a href="/settings.html" title="Settings"><i class="fas fa-pen"></i></a>
+   </div>
+ </div>
+   <div class="header-right">
+   <div class="time-box">
+   <p class="time" id="time"></p>
+   </div>
+   </div>
+ </div>
+</header>
+`);

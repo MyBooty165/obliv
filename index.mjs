b617e12f7953f711955cc091b8525f403ae83886
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-04-20 20:26:11 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,54 +1,29 @@
-/**
- * Example of serving static files and running a bare server.
- * This is a very common setup.
- */
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { createBareServer } from '@tomphttp/bare-server-node';
-import serveStatic from 'serve-static';
+import createServer from '@tomphttp/bare-server-node';
+import http from 'http';
+import nodeStatic from 'node-static';
 
-const httpServer = createServer();
 
-// Run the Bare server in the /bare/ namespace. This will prevent conflicts between the static files and the bare server.
-const bareServer = createBareServer('/bare/');
+const bare =  createServer('/bare/');
+const serve = new nodeStatic.Server('public/');
 
-// The static root is usually relative to the main script in projects that use the Bare server.
-// ie. if static.js is at /src/static.js, public will be /public/
-// ideally, you will point the public directory relative to the current working directory
-// serveStatic('./public/')
-// This would ignore the relative location of static.js
-const serve = serveStatic(
-	fileURLToPath(new URL('./public/', import.meta.url)),
-	{
-		fallthrough: false,
-	},
-);
+const server = http.createServer();
 
-httpServer.on('request', (req, res) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
 	} else {
-		serve(req, res, (err) => {
-			res.writeHead(err?.statusCode || 500, {
-				'Content-Type': 'text/plain',
-			});
-			res.end(err?.stack);
-		});
+		serve.serve(req, res);
 	}
 });
 
-httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	}else{
 		socket.end();
 	}
 });
 
-httpServer.on('listening', () => {
-	console.log('Oblivion is running');
-});
-
-httpServer.listen({
-	port: 8080,
+server.listen({
+	port: process.env.PORT || 8080,
 });

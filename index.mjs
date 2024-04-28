19a081592c576773ffcea13d68a590c28bad83dd
X-88 <126267638+MyBooty165@users.noreply.github.com>
2024-04-27 19:28:47 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -2,28 +2,44 @@ import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
-
-const bare =  createServer('/bare/');
+const bare = createServer('/bare/');
 const serve = new nodeStatic.Server('public/');
 
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) {
-		bare.routeRequest(req, res);
-	} else {
-		serve.serve(req, res);
-	}
+    const userAgent = req.headers['user-agent'];
+
+    const isMobile = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile/.test(userAgent);
+
+    if (isMobile) {
+        res.writeHead(302, { 'Location': '/static/mobile.html' });
+        res.end();
+    } else {
+        if (bare.shouldRoute(req)) {
+            bare.routeRequest(req, res);
+        } else {
+            serve.serve(req, res);
+        }
+    }
 });
 
 server.on('upgrade', (req, socket, head) => {
-	if (bare.shouldRoute(req, socket, head)) {
-		bare.routeUpgrade(req, socket, head);
-	}else{
-		socket.end();
-	}
+    const userAgent = req.headers['user-agent'];
+
+    const isMobile = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile/.test(userAgent);
+
+    if (isMobile) {
+        socket.end();
+    } else {
+        if (bare.shouldRoute(req, socket, head)) {
+            bare.routeUpgrade(req, socket, head);
+        } else {
+            socket.end();
+        }
+    }
 });
 
 server.listen({
-	port: process.env.PORT || 8080,
+    port: process.env.PORT || 8080,
 });
